896fdb9c051fd5c16acb8f6926e5dcbb44ed1ae8
Zhang Maiyun <me@maiyun.me>
2024-01-17 10:03:27 -0800

    `prandom_u32` deprecated

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -2346,12 +2346,10 @@ u64 rtw_division64(u64 x, u64 y)
 inline u32 rtw_random32(void)
 {
 #ifdef PLATFORM_LINUX
-	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0))
-	#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,1,0))
 	return get_random_u32();
-	#else
+	#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0))
 	return prandom_u32();
-	#endif
 	#elif (LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,18))
 	u32 random_int;
 	get_random_bytes( &random_int , 4 );
